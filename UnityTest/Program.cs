ea0834f24e47b2cb93b7092029afbb7e7ca1d7d6
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-02-23 17:55:58 -0300

    adiciona dependencia

--- UnityTest/Program.cs
+++ UnityTest/Program.cs
@@ -1,4 +1,6 @@
 
+using UnityTest.Services;
+
 namespace UnityTest
 {
     public class Program
@@ -7,13 +9,12 @@ namespace UnityTest
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
+            builder.Services.AddScoped<HotPotatoGameService>();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
