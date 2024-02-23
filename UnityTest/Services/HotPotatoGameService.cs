89fb5c9a10fbb3b43af754753cf96372362331d6
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-02-23 17:55:10 -0300

    faz ajustes no jogo

---
+++ UnityTest/Services/HotPotatoGameService.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace UnityTest.Services
+{
+    public class HotPotatoGameService
+    {
+        static Random random = new Random();
+        public string PlayHotPotato(int numberOfPlayers)
+        {
+            if (numberOfPlayers <= 1)
+            {
+                throw new ArgumentException("Deve haver pelo menos dois jogadores para jogar batata quente.");
+            }
+
+            Queue<int> playersQueue = new Queue<int>();
+            StringBuilder result = new StringBuilder();
+            for (int i = 1; i <= numberOfPlayers; i++)
+            {
+                playersQueue.Enqueue(i);
+            }
+
+            for (int i = 0; i < 15; i++)
+            {
+                result.AppendLine($"Rodada {i + 1}, jogadores: {string.Join(", ", playersQueue)}");
+
+                int currentPlayer = playersQueue.Dequeue();
+                playersQueue.Enqueue(currentPlayer);
+            }
+
+            int explodedPlayer = playersQueue.Dequeue();
+
+            result.AppendLine($"Jogador {explodedPlayer} explodiu.");
+
+            return result.ToString();
+        }
+    }
+}
+
